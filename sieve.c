f56987e7c710a5981bf9b0b513b8217545bb43be
Zaraksh Rahman <zarakshR@pm.me>
2023-07-14 21:26:04 +0100

    wait() for child process to terminate
    Or else we end up with orphan processes

--- sieve.c
+++ sieve.c
@@ -52,19 +52,25 @@ int main(int argc, char* argv[]) {
             close(in_pipe[PIPE_WRITE]);
             return 0;
 
-    while(true) {
+        default: break;
+    }
 
+    while (true) {
         close(in_pipe[PIPE_WRITE]);
 
         if(read(in_pipe[PIPE_READ], &prime, sizeof(int)) == 0) { return 0; }
         printf("PID: %d\tPGID:%d\tPrime: %d\n", getpid(), getpgid(0), prime);
 
         // Create a new output pipe before each fork
-        if(pipe(out_pipe) == -1) { fprintf(stderr, "pipe() failed!\n"); return 3; };
+        if (pipe(out_pipe) == -1) { ERROR_PIPE };
 
         // Flush stdout before forking or bad libc buffering things will happen
-        switch(fflush(stdout), fork()) {
-            case -1: fprintf(stderr, "fork() failed!\n"); return 2;
+        fflush(stdout);
+        int pid = fork();
+
+        switch (pid) {
+            case -1: ERROR_FORK
+
             case 0:
                 // Make out_pipe the new input pipe for when we fork again
                 in_pipe[PIPE_READ] = out_pipe[PIPE_READ];
@@ -82,7 +88,10 @@ int main(int argc, char* argv[]) {
 
                 close(in_pipe[PIPE_READ]);
                 close(out_pipe[PIPE_WRITE]);
-                wait(0);
+
+                // Wait for the child we just created or we'll end up with
+                // orphan processes which are unkillable from the terminal
+                waitpid(pid, 0, 0);
                 return 0;
         }
     }
